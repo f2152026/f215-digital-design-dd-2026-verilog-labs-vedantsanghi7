a6e4444e3cf17d7efa6b20abfb119f61b6a4cdc7
vedantsanghi7 <f20250652@pilani.bits-pilani.ac.in>
2026-08-29 18:12:23 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -17,11 +17,11 @@ module FA_Gate(
 
   wire ps, pc1, pc2;
 
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
   or  (cout, pc1, pc2);
-  xor (ps,  a,  b);
+  and (pc2, cin, ps);
+  xor (sum, cin, ps);
+  and (pc1, a,   b);
+  xor (ps,  a,   b);
 
 
 endmodule
